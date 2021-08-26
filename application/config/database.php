dd50fbc671d20a38d8ce2b870dfd1a88fb84049a
Himanshu Singh <himanshusingh8860@gmail.com>
2021-08-26 10:12:24 +0530

    code updated

--- application/config/database.php
+++ application/config/database.php
@@ -84,7 +84,7 @@ $db['default'] = array(
     'hostname' => 'qanew.cz5snxflsj5z.ap-south-1.rds.amazonaws.com',
     'username' => 'educationdoctor',
     'password' => 'PnHJM7VnasplnewqamCkAsr2v',
-    'database' => 'gharobaar_test',
+    'database' => 'local_gharobaar',
 
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
