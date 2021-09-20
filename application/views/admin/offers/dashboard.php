a40746cdf88cd72dc3ec88c46dbb21a6e7154104
pulok pandit <pulok.pandit@austere.systems>
2021-09-20 18:38:05 +0530

    changes in dashboard

--- application/views/admin/offers/dashboard.php
+++ application/views/admin/offers/dashboard.php
@@ -4,69 +4,97 @@
     thead input {
         width: 100%;
     }
+
+    .create-offer-box {
+        padding: 10px 0;
+        float: right;
+    }
+
+    .offer-heading {
+        display: inline-block;
+        font-size: 18px;
+        margin: 0;
+        line-height: 1;
+    }
 </style>
 
-<table id="offer_dashboard" class="display" style="width:100%">
-    <thead>
-        <tr>
-            <th>Offer Name</th>
-            <th>Code</th>
-            <th>Date Generated</th>
-            <th>Expiry Date</th>
-            <!-- <th>Status</th> -->
-            <th>Offer Type</th>
-            <th>Amount</th>
-            <th>%</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($offers as $offer) : ?>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="offer-heading">
+            <?php echo trans("offers"); ?>
+        </div>
+        <div class="create-offer-box">
+            <a class="btn btn-custom" href="<?php echo base_url() . 'admin/create-offers'; ?>">
+                Create New &plus;
+            </a>
+        </div>
+    </div>
+</div>
+
+
+<div>
+    <table id="offer_dashboard" class="display" style="width:100%">
+        <thead>
             <tr>
-                <td><?php echo $offer->name; ?></td>
-                <td><?php echo $offer->offer_code; ?></td>
-                <td><?php echo $offer->creation_date; ?></td>
-                <td><?php echo $offer->end_date; ?></td>
-                <!-- <td><?php echo $offer->status; ?></td> -->
-                <td><?php echo $offer->type; ?></td>
-                <td class="text-center">
-                    <?php
-                    if (!empty($offer->discount_amt)) :
-                        echo $offer->discount_amt;
-                    else : ?>
-                        -
-                    <?php endif; ?></td>
-                <td class="text-center">
-                    <?php
-                    if (!empty($offer->discount_percentage)) :
-                        echo $offer->discount_percentage;
-                    else : ?>
-                        -
-                    <?php endif; ?></td>
-                <td class="text-center">
-                    <?php if ($offer->method == "coupons") : ?>
-                        <a href="<?php echo admin_url(); ?>edit-coupon-details/<?php echo html_escape($offer->id); ?>"><input id="<?php echo html_escape($offer->id); ?>" class="favorite styled" type="button" value="Edit"></a>
-                    <?php elseif ($offer->method == "vouchers") : ?>
-                        <a href="<?php echo admin_url(); ?>edit-voucher-details/<?php echo html_escape($offer->id); ?>"><input id="<?php echo html_escape($offer->id); ?>" class="favorite styled" type="button" value="Edit"></a>
-                    <?php endif; ?>
-                </td>
+                <th>Offer Name</th>
+                <th>Code</th>
+                <th>Date Generated</th>
+                <th>Expiry Date</th>
+                <!-- <th>Status</th> -->
+                <th>Offer Type</th>
+                <th>Amount</th>
+                <th>%</th>
+                <th>Action</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-    <tfoot>
-        <tr>
-            <th>Offer Name</th>
-            <th>Code</th>
-            <th>Date Generated</th>
-            <th>Expiry Date</th>
-            <!-- <th>Status</th> -->
-            <th>Offer Type</th>
-            <th>Amount</th>
-            <th>%</th>
-            <th>Action</th>
-        </tr>
-    </tfoot>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($offers as $offer) : ?>
+                <tr>
+                    <td><?php echo $offer->name; ?></td>
+                    <td><?php echo $offer->offer_code; ?></td>
+                    <td><?php echo $offer->creation_date; ?></td>
+                    <td><?php echo $offer->end_date; ?></td>
+                    <!-- <td><?php echo $offer->status; ?></td> -->
+                    <td><?php echo $offer->type; ?></td>
+                    <td class="text-center">
+                        <?php
+                        if (!empty($offer->discount_amt)) :
+                            echo $offer->discount_amt;
+                        else : ?>
+                            -
+                        <?php endif; ?></td>
+                    <td class="text-center">
+                        <?php
+                        if (!empty($offer->discount_percentage)) :
+                            echo $offer->discount_percentage;
+                        else : ?>
+                            -
+                        <?php endif; ?></td>
+                    <td class="text-center">
+                        <?php if ($offer->method == "coupons") : ?>
+                            <a href="<?php echo admin_url(); ?>edit-coupon-details/<?php echo html_escape($offer->id); ?>"><input id="<?php echo html_escape($offer->id); ?>" class="favorite styled" type="button" value="Edit"></a>
+                        <?php elseif ($offer->method == "vouchers") : ?>
+                            <a href="<?php echo admin_url(); ?>edit-voucher-details/<?php echo html_escape($offer->id); ?>"><input id="<?php echo html_escape($offer->id); ?>" class="favorite styled" type="button" value="Edit"></a>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Offer Name</th>
+                <th>Code</th>
+                <th>Date Generated</th>
+                <th>Expiry Date</th>
+                <!-- <th>Status</th> -->
+                <th>Offer Type</th>
+                <th>Amount</th>
+                <th>%</th>
+                <th>Action</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
 
 
 
