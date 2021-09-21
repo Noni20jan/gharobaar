c696d47dcd7c17cd2154cbcea020aa4e20e17c20
Harshit Goyal <harshit.goyal@austere.co.in>
2021-09-21 17:12:46 +0530

    loyalty criteria

--- application/views/admin/loyalty/loyalty_criteria.php
+++ application/views/admin/loyalty/loyalty_criteria.php
@@ -30,64 +30,78 @@
     }
 </style>
 <!-- form start -->
-<?php echo form_open('admin_controller/loyalty_program_submit'); ?>
-<div class="col-12 coupons-from-holder">
-    <div class="form-group">
-        <div class="row">
-            <div class="col-sm-6"><label>User Type:</label></div>
-            <div class="col-sm-6">
-                <select name="user_type" class="form-control auth-form-input" id="offer-type">
-                    <?php foreach ($user_type as $user_type) { ?>
-                        <option value="<?php echo $user_type->id; ?>"><?php echo $user_type->lookup_code; ?></option>
-                    <?php } ?>
-                </select>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title"><?php echo trans('send_email_members'); ?></h3>
             </div>
-        </div>
-    </div>
+            <!-- /.box-header -->
 
-    <div class="form-group">
-        <div class="row">
-            <div class="col-sm-12"><label>KPI</label></div>
-            <div class="input_fields_container">
-                <button class="btn btn-sm btn-primary add_more_button">Add More Rows</button>
-            </div>
-        </div>
-    </div>
-    <div class="row form-group">
-        <div class="col-sm-3">
-            <label>NAME:</label>
-            <input type='text' name="kpi_name" class="form-control auth-form-input" value="" placeholder="Name" required>
-        </div>
-        <div class="col-sm-3">
-            <label>KPI Type:</label>
-            <select class="form-control auth-form-input" name="kpi_type" id="kpi_type" onchange="child_parent_name();">
-                <option value="parent">Parent</option>
-                <option value="child">Child</option>
-                <option value="individual">Individual</option>
-            </select>
-        </div>
-        <div class="col-sm-3">
-            <label>Parent KPI</label>
-            <input type='text' name="parent_name" id="parent_name" placeholder="Parent KPI" class="form-control auth-form-input" value="">
-        </div>
-        <div class="col-sm-2">
-            <label>Weightage</label>
-            <input type="number" maxlength="3" name="weightage" placeholder="Weightage" class="form-control auth-form-input" value="" required>
-        </div>
-        <!-- <div class="col-sm-4"><label>Formula/SQL</label>
+            <!-- form start -->
+            <div class="box-body">
+                <?php echo form_open('admin_controller/loyalty_program_submit'); ?>
+                <div class="col-12 coupons-from-holder">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-sm-6"><label>User Type:</label></div>
+                            <div class="col-sm-6">
+                                <select name="user_type" class="form-control auth-form-input" id="offer-type">
+                                    <?php foreach ($user_type as $user_type) { ?>
+                                        <option value="<?php echo $user_type->id; ?>"><?php echo $user_type->lookup_code; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-sm-12"><label>KPI</label></div>
+                            <div class="input_fields_container">
+                                <button class="btn btn-sm btn-primary add_more_button">Add More Rows</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-3">
+                            <label>NAME:</label>
+                            <input type='text' name="kpi_name" class="form-control auth-form-input" value="" placeholder="Name" required>
+                        </div>
+                        <div class="col-sm-3">
+                            <label>KPI Type:</label>
+                            <select class="form-control auth-form-input" name="kpi_type" id="kpi_type" onchange="child_parent_name();">
+                                <option value="parent">Parent</option>
+                                <option value="child">Child</option>
+                                <option value="individual">Individual</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-3">
+                            <label>Parent KPI</label>
+                            <input type='text' name="parent_name" id="parent_name" placeholder="Parent KPI" class="form-control auth-form-input" value="">
+                        </div>
+                        <div class="col-sm-2">
+                            <label>Weightage</label>
+                            <input type="number" maxlength="3" name="weightage" placeholder="Weightage" class="form-control auth-form-input" value="" required>
+                        </div>
+                        <!-- <div class="col-sm-4"><label>Formula/SQL</label>
 
             <input type='text' placeholder="Formula" name="formula" class="form-control auth-form-input" value="" required>
         </div> -->
-    </div>
-    <div class="input_fields_container1">
+                    </div>
+                    <div class="input_fields_container1">
+                    </div>
+                </div>
+                <div class=" box-footer">
+                    <button type="submit" class="btn btn-primary pull-right"><?php echo trans('submit'); ?></button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 
 
-<div class=" box-footer">
-    <button type="submit" class="btn btn-primary pull-right"><?php echo trans('submit'); ?></button>
-</div>
 
 
 <?php echo form_close(); ?>
