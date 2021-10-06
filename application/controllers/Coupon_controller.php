d8f797821e5c3d3851823dea575b59cd0370d752
Deepansh Sharma <deepansh.sharma@austere.co.in>
2021-10-06 18:24:22 +0530

    coupon fixed

--- application/controllers/Coupon_controller.php
+++ application/controllers/Coupon_controller.php
@@ -170,7 +170,6 @@ class Coupon_controller extends Admin_Core_Controller
         //get paginated products
 
         $data["coupons"] = $this->offer_model->show_vouchers_data();
-        var_dump($data["coupons"]);
         $this->load->view('admin/includes/_header', $data);
         $this->load->view('admin/offers/user_vouchers', $data);
         $this->load->view('admin/includes/_footer');
