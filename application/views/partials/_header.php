3a4a21eca9e56efc8e2698c49c60dcd852ada0cc
Himanshu Singh <himanshusingh8860@gmail.com>
2021-10-14 18:58:14 +0530

    code updated for google analytics

--- application/views/partials/_header.php
+++ application/views/partials/_header.php
@@ -5420,6 +5420,19 @@
     </script>
     <!-- End Google Tag Manager -->
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-199516838-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-199516838-1');
+    </script>
+
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGZRFDN" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
