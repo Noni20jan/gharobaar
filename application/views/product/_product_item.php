4ec538c2087098f6af07de0b495d3c4b5a0c9a10
Siddharth Sharma <siddharth.sharma@austere.co.in>
2021-11-17 18:20:40 +0530

    reviews corrected

--- application/views/product/_product_item.php
+++ application/views/product/_product_item.php
@@ -32,7 +32,8 @@
         transform: scale(1.25);
     }
 </style>
-
+<?php //var_dump($product); 
+?>
 <div class="product-item">
     <div class="row-custom<?php echo (!empty($product->image_second)) ? ' product-multiple-image' : ''; ?>">
         <a class="item-wishlist-button item-wishlist-enable <?php echo (is_product_in_wishlist($product) == 1) ? 'item-wishlist' : ''; ?>" data-product-id="<?php echo $product->id; ?>"></a>
@@ -109,12 +110,15 @@
         <?php if ($this->general_settings->reviews == 1) : ?>
             <div class="product-details-review">
                 <?php $this->load->view('partials/_review_stars', ['review' => $product->rating]); ?>
+                <?php $count_star = $this->review_model->get_review_count($product->id); ?>
+                <span><b> (<?php echo $count_star; ?>)</b> </span>
             </div>
         <?php endif; ?>
         <?php if ($product->discount_rate != '0') : ?>
-            <!-- <div class="discount-rate" style="float:right; color: green; margin-top: -15%">
-                <?php echo discount_rate_format($product->discount_rate) ?>
-            </div> -->
+            <!-- <div class="discount-rate" style="float:right; color: green; margin-top: -15%">-->
+            <?php echo discount_rate_format($product->discount_rate) ?>
+
+            <!-- </div> -->
         <?php endif; ?>
         <div class="item-meta">
             <?php $this->load->view('product/_price_product_item', ['product' => $product]); ?>
