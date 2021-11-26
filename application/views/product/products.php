fbb4d978c4ef7346bfff6951371c178a3046fc0a
Himanshu Singh <himanshusingh8860@gmail.com>
2021-11-26 14:50:48 +0530

    code updated for max CDN change

--- application/views/product/products.php
+++ application/views/product/products.php
@@ -2005,7 +2005,7 @@ foreach ($sellers as $seller) {
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 <script type="text/javascript"></script>
 <script>
     var page = 1;
