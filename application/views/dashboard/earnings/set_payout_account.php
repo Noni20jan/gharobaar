df0893767f91232364470c11b5cc62c9747083a6
Deepansh Sharma <deepansh.sharma@austere.co.in>
2021-12-22 23:22:08 +0530

    validate bank details

--- application/views/dashboard/earnings/set_payout_account.php
+++ application/views/dashboard/earnings/set_payout_account.php
@@ -154,7 +154,7 @@
                                         <div class="row Brand-1" style="margin-bottom: 3%;">
                                             <div class="col-md-3"><label id="formlabel2">Account Number<span class="Validation_error"> *</span></label></div>
                                             <div class="col-md-9 Brand-name">
-                                                <input type='password' name="account_number" id="account_number" class="form-control auth-form-input" minlength="9" required onkeyup="checkLength()">
+                                                <input type='password' name="account_number" id="account_number" class="form-control auth-form-input" value="<?php echo html_escape($this->auth_user->account_number); ?>" minlength="9" required onkeyup="checkLength()">
                                                 <span style="color: red;" id="acc_number"></span>
                                             </div>
                                         </div>
