f1d3a1db8cc7f75182ddf2e448036cee3dad9879
shahnaz Akhtar <shahnaz.akhtar@austere.co.in>
2021-12-23 19:59:19 +0530

    export datatables

--- application/views/admin/offers/category_coupon.php
+++ application/views/admin/offers/category_coupon.php
@@ -1,4 +1,16 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.print.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+</link>
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.1.0/css/buttons.dataTables.min.css">
+</link>
 <style>
     .hide-div {
         display: none;
@@ -89,6 +101,10 @@
                             'orderable': false,
                             'className': 'dt-body-center',
                         }],
+                        // dom: 'lBfrtip',
+                        // buttons: [
+                        //     'excel'
+                        // ],
                         'order': [1, 'asc']
                     });
                     table.clear().destroy();
@@ -186,9 +202,15 @@
                         'orderable': false,
                         'className': 'dt-body-center',
 
-                    }],
+                    }],  
+                    dom: 'lBfrtip',
+                        buttons: [
+                            'excel'
+                        ],
+                        
                     'order': [1, 'asc']
                 });
+                // table.clear().destroy();
 
 
 
--- application/views/admin/offers/vouchers_user.php
+++ application/views/admin/offers/vouchers_user.php
@@ -1,10 +1,22 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
 </link>
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> -->
 
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.print.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+</link>
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.1.0/css/buttons.dataTables.min.css">
+</link>
 <style>
     .nxt-cancel-btns {
         text-align: center;
@@ -206,6 +218,10 @@
                             'className': 'dt-body-center',
 
                         }],
+                        dom: 'lBfrtip',
+                        buttons: [
+                            'excel'
+                        ],
                         'order': [1, 'asc']
                     });
 
@@ -223,3 +239,13 @@
             });
         });
     </script>
+    <!-- <script>
+        $(document).ready(function() {
+            $('#example1').DataTable({
+                dom: 'lBfrtip',
+                buttons: [
+                    'excel'
+                ]
+            });
+        });
+    </script> -->
