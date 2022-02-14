342f6360465ed7dc2acaf8b0370524faa67b775c
Deepansh Sharma <sharmadeepansh852@gmail.com>
2022-02-14 11:32:42 +0530

    cart changes

--- application/views/product/_product_item.php
+++ application/views/product/_product_item.php
@@ -1,4 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap-notify.js"></script>
+
 <style>
     .zoom {
         transition: transform .2s;
