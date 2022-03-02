716c094f0a7c6108cf79ea7d3d4d88fdc8303719
Siddharth Sharma <siddharth.sharma@austere.co.in>
2022-03-02 16:04:33 +0530

    code updated for exapandable order

--- application/views/admin/order/orders.php
+++ application/views/admin/order/orders.php
@@ -168,16 +168,14 @@
 		}
 		});
 </script>
+<!-- script for expandable orders -->
 <script>
-    // $(document).ready(function() {
+    
         var dataa=1;
-    // });
-
         function order_summary_on_orders(order_number) {
             if(document.getElementById('table_'+order_number).rows.length==0){
 
             
-        //   if(dataa==1){
                     var a = {
                         
                     order_id:order_number,
@@ -192,9 +190,23 @@
                             dataa=0;
                             var Json_data = JSON.parse(response);
                             var len = Json_data.length;
+
                 if (len != 0) {
+                    var time_ago_format;
                     for (var i = 0; i < len; i++) {
-                        $('#table_'+order_number).append("<tr><td>" + Json_data[i].product_id + "</td><td>" + Json_data[i].product_title + "</td><td>" + Json_data[i].product_unit_price/100+'INR' + "</td><td>" + Json_data[i].product_quantity + "</td><td>" + Json_data[i].product_gst/100+'INR' + "</td><td>" + Json_data[i].product_shipping_cost/100+'INR' + "</td><td>" + Json_data[i].product_total_price + "</td><td>" + Json_data[i].order_status + "</td><td>"+ Json_data[i].updated_at + "</td><td>")
+                        var new_order_status="";
+                        if(Json_data[i].order_status=="cancelled_by_user"){
+                            new_order_status="Cancelled By User";
+                        }
+                        else if(Json_data[i].order_status=="completed"){
+                            new_order_status="Completed";
+                        }
+                        else if(Json_data[i].order_status=="processing"){
+                            new_order_status="Processing";
+                        }
+                        var time_format =Json_data[i].updated_at;
+                        time_ago_format= time_ago(time_format);
+                        $('#table_'+order_number).append("<tr><td>" + Json_data[i].product_id + "</td><td>" + Json_data[i].product_title + "</td><td>" +"₹"+ Json_data[i].product_unit_price/100 + "</td><td>" + Json_data[i].product_quantity + "</td><td>" +"₹"+ Json_data[i].product_gst/100 + "</td><td>" +"₹"+ Json_data[i].product_shipping_cost/100 + "</td><td>" +"₹"+ Json_data[i].product_total_price/100 + "</td><td>" + new_order_status + "</td><td>"+ time_ago_format + "</td><td>")
                     }
                 }
                         }
@@ -203,4 +215,64 @@
                     );
                 }
             }
+
+    function time_ago(time) {
+
+        switch (typeof time) {
+        case 'number':
+            break;
+        case 'string':
+            time = +new Date(time);
+            break;
+        case 'object':
+            if (time.constructor === Date) time = time.getTime();
+            break;
+        default:
+            time = +new Date();
+        }
+        var time_formats = [
+        [60, 'seconds', 1], // 60
+        [120, '1 minute ago', '1 minute from now'], // 60*2
+        [3600, 'minutes', 60], // 60*60, 60
+        [7200, '1 hour ago', '1 hour from now'], // 60*60*2
+        [86400, 'hours', 3600], // 60*60*24, 60*60
+        [172800, '1 day ago', 'Tomorrow'], // 60*60*24*2
+        [604800, 'days', 86400], // 60*60*24*7, 60*60*24
+        [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+        [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
+        [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+        [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+        [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+        [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+        [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+        [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+        ];
+        var seconds = (+new Date() - time) / 1000,
+        token = 'ago',
+        list_choice = 1;
+
+        if (seconds == 0) {
+        return 'Just now'
+        }
+        if (seconds < 0) {
+        seconds = Math.abs(seconds);
+        token = 'from now';
+        list_choice = 2;
+        }
+        var i = 0,
+        format;
+        while (format = time_formats[i++])
+        if (seconds < format[0]) {
+            if (typeof format[2] == 'string')
+            return format[list_choice];
+            else
+            return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+        }
+        return time;
+        }
+
+        // var aDay = 24 * 60 * 60 * 1000;
+        // console.log(time_ago(new Date(Date.now() - aDay)));
+        // console.log(time_ago(new Date(Date.now() - aDay * 2)));
 </script>
+<!-- script end for expandable orders -->
