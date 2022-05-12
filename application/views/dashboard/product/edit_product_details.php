9b99169e6752adc4c58ccf14d6d0820c8b080dc8
Akshat sharma <akshat.sharma@austere.co.in>
2022-05-12 11:56:13 +0530

    validation for address length

--- application/views/dashboard/product/edit_product_details.php
+++ application/views/dashboard/product/edit_product_details.php
@@ -210,26 +210,26 @@
                         <?php $this->load->view("dashboard/product/_edit_price", ['user' => $user]); ?>
 
                         <?php
-                            $categories = get_parent_categories_tree($product->category_id, false);
-                            if ($categories[1]->id == 38 && $categories[1]->slug == 'apparel') :
-                            ?>
-                                <div class="form-box">
-                                    <div class="row ">
-                                        <div class="col-sm-12 col-md-6 m-b-sm-15">
-                                            <label><?php echo ('Product Suitable For'); ?></label><span class="Validation_error"> *</span>
-                                            
-                                                <select name="suitable_for_kids" id="suitable_for" class="form-control custom-select m-0" required>
-                                                    <option disabled selected value> -- Select an option -- </option>
-                                                    <option value="male" <?php echo ($product->suitable_for == "male") ? 'selected' : ''; ?>>Male</option>
-                                                    <option value="female" <?php echo ($product->suitable_for == "female") ? 'selected' : ''; ?>>Female</option>
-                                                    <option value="unisex" <?php echo ($product->suitable_for == "unisex") ? 'selected' : ''; ?>>Unisex</option>
-                                                </select>
-                                            </div>
-                                        
+                        $categories = get_parent_categories_tree($product->category_id, false);
+                        if ($categories[1]->id == 38 && $categories[1]->slug == 'apparel') :
+                        ?>
+                            <div class="form-box">
+                                <div class="row ">
+                                    <div class="col-sm-12 col-md-6 m-b-sm-15">
+                                        <label><?php echo ('Product Suitable For'); ?></label><span class="Validation_error"> *</span>
+
+                                        <select name="suitable_for_kids" id="suitable_for" class="form-control custom-select m-0" required>
+                                            <option disabled selected value> -- Select an option -- </option>
+                                            <option value="male" <?php echo ($product->suitable_for == "male") ? 'selected' : ''; ?>>Male</option>
+                                            <option value="female" <?php echo ($product->suitable_for == "female") ? 'selected' : ''; ?>>Female</option>
+                                            <option value="unisex" <?php echo ($product->suitable_for == "unisex") ? 'selected' : ''; ?>>Unisex</option>
+                                        </select>
                                     </div>
+
                                 </div>
-                            <?php endif;
-                            ?>
+                            </div>
+                        <?php endif;
+                        ?>
 
                         <?php if ($this->form_settings->shipping == 1 && $product->product_type == 'physical') : ?>
                             <div class="form-box">
@@ -1234,7 +1234,8 @@
                                                 <div class="col-12 col-sm-4 m-b-15">
                                                     <label class="control-label">House no./Building no./Area<span class="Validation_error"> *</span>
                                                     </label>
-                                                    <input type="text" name="house_no" id="product_address" minlength="10" class="form-control auth-form-input" placeholder="House no./Building no./Area" value="<?php echo empty($product->product_address) ? html_escape($user->house_no) : html_escape($product->product_address); ?>" required>
+                                                    <input type="text" name="house_no" maxlength="74" id="product_address" onkeyup="checklength(this)" minlength="10" class="form-control auth-form-input" placeholder="House no./Building no./Area" value="<?php echo empty($product->product_address) ? html_escape($user->house_no) : html_escape($product->product_address); ?>" required>
+                                                    <span class="word_limit" id="word_limit" style="display:none ; color:red">*maxmimum 74 only*</span>
                                                     <p class="Validation_error" id="house_no_p1"></p>
                                                 </div>
                                                 <div class="col-12 col-sm-4 m-b-15">
@@ -1939,10 +1940,29 @@
                     allRequired = false;
                 }
             });
+
+
             if (!allRequired) {
                 $("#button_with_submit").trigger("click");
             } else {
                 $("#button_to_open_modal").trigger("click");
             }
         });
+    </script>
+    <script>
+        function checklength(el) {
+            var limit = $('#product_address').val();
+            $button = $('#button_to_submit')
+            if (el.value.length >= 74) {
+                $("#word_limit").attr("style", "display:block")
+                $button.prop('disabled', 'disabled');
+
+            } else {
+                $("#word_limit").attr("style", "display:none")
+
+                $button.prop('disabled', false);
+
+            }
+
+        }
     </script>
