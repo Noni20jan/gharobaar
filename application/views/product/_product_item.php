4e91fe3f4294fb190bf8d6cf4571485402acf722
Himanshu Singh <himanshusingh8860@gmail.com>
2022-05-26 18:04:31 +0530

    code updated for product items

--- application/views/product/_product_item.php
+++ application/views/product/_product_item.php
@@ -25,22 +25,6 @@
         }
     }
 
-    @media all and (device-width: 768px) and (device-height: 1024px) and (orientation:portrait) {
-        .product-item .img-product {
-            width: 100%;
-            height: 150px;
-
-        }
-    }
-
-    @media all and (device-width: 1024px) and (device-height: 768px) and (orientation:landscape) {
-        .product-item .img-product {
-            width: 100%;
-            height: 150px;
-        }
-    }
-
-
     .zoom:hover {
         -ms-transform: scale(1.25);
         /* IE 9 */
