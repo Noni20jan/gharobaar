82111d393556ae5a181ce629f5b6e1f355c0d220
Admin <admin@austere.co.in>
2022-09-21 14:42:57 +0530

    db file change

--- application/config/database.php
+++ application/config/database.php
@@ -88,7 +88,7 @@ $db['default'] = array(
     // 'database' => 'local_gharobaar',
 
     'hostname' => '1.23.242.234',
-    'database' => 'local_gharobaar',
+    'database' => 'test_gharobaar',
     'username' => 'root',
     'password' => 'AWFDWD#$%^3452$EFGGsdsa',
     'dbdriver' => 'mysqli',
