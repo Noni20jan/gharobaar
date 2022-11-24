05f24848ec11c5bed5291aead379e09282ed7d26
Swaraj Misra <swaraj.misra@austere.co.in>
2022-11-24 12:38:50 +0530

    Swaraj calculation

--- application/views/cart/_order_summary.php
+++ application/views/cart/_order_summary.php
@@ -130,7 +130,7 @@
         </div>
         <p class="m-t-30">
 
-            <strong><?php echo trans("subtotal"); ?><span class="float-right" id="sub_total"><?php echo price_formatted_without_round(floor($cart_item->listing_price - ($cart_item->discount_amount * 100))*$cart_item->quantity, $this->payment_settings->default_currency); ?>/-</span></strong>
+            <strong><?php echo trans("subtotal"); ?><span class="float-right" id="sub_total"><?php echo price_formatted_without_round(floor($cart_item->listing_price-($cart_item->discount_amount * 100))*$cart_item->quantity, $this->payment_settings->default_currency); ?>/-</span></strong>
         </p>
         <!-- <?php if (!empty($cart_total->gst)) : ?>
             <p>
