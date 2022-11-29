ee02b7af7620a733eafb7862ee4185bd152ba915
Swaraj Misra <swaraj.misra@austere.co.in>
2022-11-29 11:24:53 +0530

    subtotal correction

--- application/views/cart/_order_summary.php
+++ application/views/cart/_order_summary.php
@@ -4,6 +4,8 @@
         margin-top: 70px !important;
     }
 </style>
+<?php $cto=$cart_total->order_total;?>
+                    
 <div id="cart" class="col-sm-12 col-lg-5 order-summary-container">
     <h2 class="cart-section-title" id="cart_quantity"><?php echo trans("order_summary"); ?> (<?php echo get_cart_product_count(); ?>)</h2>
     <!-- <a href="<?php echo generate_url("cart"); ?>">
@@ -130,7 +132,8 @@
         </div>
         <p class="m-t-30">
 
-            <strong><?php echo trans("subtotal"); ?><span class="float-right" id="sub_total"><?php echo price_formatted_without_round(floor($cart_item->listing_price-($cart_item->discount_amount * 100))*$cart_item->quantity, $this->payment_settings->default_currency); ?>/-</span></strong>
+            <strong><?php echo trans("subtotal"); ?><span class="float-right" id="sub_total"><?php echo price_formatted_without_round(floor($cart_total->total_price), $this->payment_settings->default_currency); ?>/-</span></strong>
+       
         </p>
         <!-- <?php if (!empty($cart_total->gst)) : ?>
             <p>
@@ -247,8 +250,11 @@
                 <p>
                 <p>
                     <!-- <strong><?php echo trans("total"); ?><span id="order_total" class="float-right"><?php echo price_formatted_without_round($cart_total->total_price, $this->payment_settings->default_currency); ?>/-</span></strong> -->
+                    <?php //var_dump($cart_total->order_total);?>
+                    <?php //var_dump($cart_total->order_total==$cto);?>
                     <?php if($cart_total->total_price<100000):?>
-                    <strong><?php echo trans("total"); ?><span id="order_total" class="float-right"><?php echo price_formatted_without_round($cart_total->total_price, $this->payment_settings->default_currency); ?>/-</span></strong>
+                    
+                    <strong><?php echo trans("total"); ?><span id="order_total" class="float-right"><?php echo price_formatted_without_round($cart_total->total+10000, $this->payment_settings->default_currency); ?>/-</span></strong>
                     <?php else:?>
                         <strong><?php echo trans("total"); ?><span id="order_total" class="float-right"><?php echo price_formatted_without_round($cart_total->total_price, $this->payment_settings->default_currency); ?>/-</span></strong>
             <?php endif; ?>
