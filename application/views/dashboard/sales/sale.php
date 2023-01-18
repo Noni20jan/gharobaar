8c1e3274e6e9b07eaa3b6bde720edd5aa1dd88aa
Deepali <deepali@austere.co.in>
2023-01-18 15:43:11 +0530

    Deepali : Task 401 Testing of scheduling shipment status

--- application/views/dashboard/sales/sale.php
+++ application/views/dashboard/sales/sale.php
@@ -434,7 +434,6 @@ endif;
                                 <th scope="col"><?php echo trans("status"); ?></th>
                                 <th scope="col"><?php echo trans("reject_reason"); ?></th>
                                 <th scope="col"><?php echo trans("payment_status"); ?></th>
-
                                 <th scope="col"><?php echo trans("updated"); ?></th>
                                 <th scope="col"><?php echo trans("options"); ?></th>
                             </tr>
@@ -707,7 +706,54 @@ endif;
                                                 echo time_ago($item->updated_at);
                                             } ?>
                                         </td>
-                                        <td style="width: 25%">
+                                        <td id="table_show" style="width: 25%">
+                                        <div class='response'></div>
+
+<?php if ($item->product_delivery_partner == "SHIPROCKET") : ?>
+    <?php $shiprocket_order_details = get_shiprocket_order_details($order->id, $item->product_id); ?>
+
+    <?php if (!empty($shiprocket_order_details) && $shiprocket_order_details->is_active == 1) : ?>
+        <?php if (!empty($shiprocket_order_details->awb_code)) : ?>
+            <p class="m-b-5">
+                <button type="button" style="width:100%; border-radius:20px; color:white" class="btn btn-md btn-block btn-warning"><a style="color:white" href="<?php echo $shiprocket_order_details->manifest_url ?>">Download Manifest</a></button>
+            </p>
+            <p class="m-b-5">
+                <button type="button" style="width:100%; border-radius:20px; color:white" class="btn btn-md btn-block btn-dark"><a style="color:white" href="<?php echo $shiprocket_order_details->label_url ?>">Download Label</a></button>
+            </p>
+        <?php endif; ?>
+
+        <p class="m-b-5">
+            <button type="button" style="width:100%;" class="btn btn-md btn-block btn-primary" data-toggle="modal"><a style="color:white" href="<?php echo base_url(); ?>dashboard/track_status/<?php echo $shiprocket_order_details->awb_code ?>"> Track Status </a></button>
+        </p>
+        <?php if (!empty($shiprocket_order_details->awb_code)) : ?>
+            <p class="m-b-5">
+                <?php //if ($this->general_settings->cancel_shipment == 1) { 
+                ?>
+                <button type="button" class="btn btn-md btn-block btn-danger" onclick="shiprocket_cancel_order('<?php echo $shiprocket_order_details->shipment_order_id; ?>','Are you sure you want to cancel the shipment?')"> Cancel Shipment </button>
+                <?php //} 
+                ?>
+            </p>
+        <?php endif; ?>
+    <?php endif; ?>
+
+
+
+<?php elseif ($item->product_delivery_partner == "NOW-BIKES") : ?>
+
+    <?php $now_bike_data = get_nowBike_order_details($order->order_number, $item->id); ?>
+
+    <?php if (!empty($now_bike_data)) : ?>
+        <p class="m-b-5">
+            <button type="button" class="btn btn-md btn-block btn-primary" onclick="nowBike_get_order_details('<?php echo $now_bike_data->now_bike_id; ?>',$(this))"> Check Status </button>
+        </p>
+        <p class="m-b-5">
+            <button type="button" class="btn btn-md btn-block btn-danger" onclick="nowBike_cancle_order('<?php echo $now_bike_data->now_bike_id; ?>','Are you sure you want to cancel the shipment?')"> Cancel Now-Bike Shipment </button>
+        </p>
+    <?php endif; ?>
+
+<?php endif; ?>
+                                        </td>
+                                        <td id="table_hide" style="width: 25%">
                                             <?php if ($item->order_status == "completed") : ?>
                                                 <strong class="font-600"><i class="icon-check"></i>&nbsp;<?php echo trans("approved"); ?></strong>
                                             <?php elseif ($item->order_status == "rejected") : ?>
@@ -740,51 +786,7 @@ endif;
                                                     </div>
                                                     <!-- Image loader -->
 
-                                                    <div class='response'></div>
-
-                                                    <?php if ($item->product_delivery_partner == "SHIPROCKET") : ?>
-                                                        <?php $shiprocket_order_details = get_shiprocket_order_details($order->id, $item->product_id); ?>
-
-                                                        <?php if (!empty($shiprocket_order_details) && $shiprocket_order_details->is_active == 1) : ?>
-                                                            <?php if (!empty($shiprocket_order_details->awb_code)) : ?>
-                                                                <p class="m-b-5">
-                                                                    <button type="button" style="width:100%; border-radius:20px; color:white" class="btn btn-md btn-block btn-warning"><a style="color:white" href="<?php echo $shiprocket_order_details->manifest_url ?>">Download Manifest</a></button>
-                                                                </p>
-                                                                <p class="m-b-5">
-                                                                    <button type="button" style="width:100%; border-radius:20px; color:white" class="btn btn-md btn-block btn-dark"><a style="color:white" href="<?php echo $shiprocket_order_details->label_url ?>">Download Label</a></button>
-                                                                </p>
-                                                            <?php endif; ?>
-
-                                                            <p class="m-b-5">
-                                                                <button type="button" style="width:100%;" class="btn btn-md btn-block btn-primary" data-toggle="modal"><a style="color:white" href="<?php echo base_url(); ?>dashboard/track_status/<?php echo $shiprocket_order_details->awb_code ?>"> Track Status </a></button>
-                                                            </p>
-                                                            <?php if (!empty($shiprocket_order_details->awb_code)) : ?>
-                                                                <p class="m-b-5">
-                                                                    <?php //if ($this->general_settings->cancel_shipment == 1) { 
-                                                                    ?>
-                                                                    <button type="button" class="btn btn-md btn-block btn-danger" onclick="shiprocket_cancel_order('<?php echo $shiprocket_order_details->shipment_order_id; ?>','Are you sure you want to cancel the shipment?')"> Cancel Shipment </button>
-                                                                    <?php //} 
-                                                                    ?>
-                                                                </p>
-                                                            <?php endif; ?>
-                                                        <?php endif; ?>
-
-
-
-                                                    <?php elseif ($item->product_delivery_partner == "NOW-BIKES") : ?>
-
-                                                        <?php $now_bike_data = get_nowBike_order_details($order->order_number, $item->id); ?>
-
-                                                        <?php if (!empty($now_bike_data)) : ?>
-                                                            <p class="m-b-5">
-                                                                <button type="button" class="btn btn-md btn-block btn-primary" onclick="nowBike_get_order_details('<?php echo $now_bike_data->now_bike_id; ?>',$(this))"> Check Status </button>
-                                                            </p>
-                                                            <p class="m-b-5">
-                                                                <button type="button" class="btn btn-md btn-block btn-danger" onclick="nowBike_cancle_order('<?php echo $now_bike_data->now_bike_id; ?>','Are you sure you want to cancel the shipment?')"> Cancel Now-Bike Shipment </button>
-                                                            </p>
-                                                        <?php endif; ?>
-
-                                                    <?php endif; ?>
+                                                   
                                                     <p class="m-b-5">
 
 
@@ -843,11 +845,6 @@ endif;
                                                             <!-- <p> <?php echo trans("pickup_token_number") ?>:&nbsp;<?php echo $chk->pickup_token_number; ?></a></p> -->
                                                         <?php endif; ?>
                                                         <p> <?php echo trans("shipment_id") ?>:&nbsp;<?php echo $chk->shipment_id; ?></a></p>
-
-
-
-
-
 
                                                     </div>
                                                 </td>
@@ -1376,6 +1373,8 @@ endforeach; ?>
             // $("#schedule_multiple_products").modal('hide');
 
         }
+        document.getElementById("table_hide").style.display= "none";
+        document.getElementById("table_show").style.display= "block";
     }
 
     function wrapper_multiple_product(products_array, order_items_array) {
